0e9f3c81faab6d9839b9d2b3a19fab8442a9116e
Manlikenonso1k <victorynonso9@gmail.com>
2026-06-01 16:20:04 +0100

    Fix: Allow BackedEnum|string|null navigationIcon in ConnectAccounts

--- app/Filament/Pages/ConnectAccounts.php
+++ app/Filament/Pages/ConnectAccounts.php
@@ -6,7 +6,13 @@ use Filament\Pages\Page;
 
 class ConnectAccounts extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-link';
+    /**
+     * Allow string or BackedEnum (matches parent type)
+     *
+     * @var \BackedEnum|string|null
+     */
+    protected static \BackedEnum|string|null $navigationIcon = 'heroicon-o-link';
+
     protected static ?string $navigationLabel = 'Platform Integrations';
     protected static string $view = 'filament.pages.connect-accounts';
 }
